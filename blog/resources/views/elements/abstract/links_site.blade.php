690740f8115a37bd4502dd4f1c65460fae8f70e9
Marco Hernandez <marco.hernandez@globant.com>
2016-10-28 17:56:12 -0400

    added new article and image

--- blog/resources/views/elements/abstract/links_site.blade.php
+++ blog/resources/views/elements/abstract/links_site.blade.php
@@ -1,10 +1,8 @@
 <div class="col-two tab-1-3 mob-1-2 site-links">
-	<h4>Site Links</h4>
+	<h4>{{ trans('nav.site_links') }}</h4>
 	<ul>
-		<li><a href="#">About Us</a></li>
-		<li><a href="#">Blog</a></li>
-		<li><a href="#">FAQ</a></li>
-		<li><a href="#">Terms</a></li>
-		<li><a href="#">Privacy Policy</a></li>
+		<li><a href="{{ URL::to(LaravelLocalization::getCurrentLocale().'/') }}">{{ trans('nav.home') }}</a></li>
+		<li><a href="{{ URL::to(LaravelLocalization::getCurrentLocale().'/about') }}">{{ trans('nav.about') }}</a></li>
+		<li><a href="{{ URL::to(LaravelLocalization::getCurrentLocale().'/contact') }}">{{ trans('nav.contact') }}</a></li>
 	</ul>
 </div>
